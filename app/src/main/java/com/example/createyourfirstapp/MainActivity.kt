d50c385c23b26fef6107605018cea89101a4e182
Mia Smaldone <eufosm@libero.it>
2023-02-06 16:30:30 +0100

    RecycleView

--- app/src/main/java/com/example/createyourfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/createyourfirstapp/MainActivity.kt
@@ -9,17 +9,27 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.createyourfirstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    val cities = listOf(City("Rome"), City("Berlin"), City("New York"), City("London"))
+    //val cities = listOf(City("Rome"), City("Berlin"), City("New York"), City("London"))
+
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
+       // val list = findViewById<RecyclerView>(R.id.city_list)
+       // val adapter = CityAdapter(cities)
+       // list.adapter = adapter
+       // list.layoutManager = LinearLayoutManager(this)
 
      binding = ActivityMainBinding.inflate(layoutInflater)
      setContentView(binding.root)
